a1e21704186b8e46f161a5b29c3f7bfeace5ca09
Ben Webb <bjwebb67@googlemail.com>
2017-11-28 11:08:21 +0000

    [#3] Output anchors based on field path

--- sphinxcontrib/jsonschema.py
+++ sphinxcontrib/jsonschema.py
@@ -110,7 +110,7 @@ class JSONSchemaDirective(Directive):
 
     def row(self, prop, tbody):
         row = nodes.row()
-        cell = nodes.entry('', nodes.literal('', nodes.Text(prop.name)), morecols=1)
+        cell = nodes.entry('', nodes.target(ids=[prop.name], names=[prop.name]), nodes.literal('', nodes.Text(prop.name)), morecols=1)
         row += cell
         row += self.cell(prop.type)
         row += self.cell(prop.format or '')
